96493405e7d19ff4e138edd9e654326046f75a52
Harrison Bacordo <harrisonbacordo@gmail.com>
2020-10-29 20:04:08 -0400

    Spotless integration; Laying foundation

--- app/src/main/java/com/harrisonbacordo/flatmate/ui/FlatMateApp.kt
+++
@@ -1,2 +0,0 @@
-package com.harrisonbacordo.flatmate.ui
-
--- app/src/main/java/com/harrisonbacordo/flatmate/ui/FlatMateEntryPoint.kt
+++ app/src/main/java/com/harrisonbacordo/flatmate/ui/FlatMateEntryPoint.kt
@@ -13,14 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.harrisonbacordo.flatmate.ui.theme
+package com.harrisonbacordo.flatmate.ui
 
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Shapes
-import androidx.compose.ui.unit.dp
+import androidx.activity.OnBackPressedDispatcher
 
-val shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
-)
+fun FlatMateEntryPoint(backDispatcher: OnBackPressedDispatcher) {
+}
